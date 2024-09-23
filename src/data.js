f23c6991af61b7926b5bb2459b5e47409542afcd
Andres Astudillo <andresdiastullo@gmail.com>
2024-09-23 20:50:27 -0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,43 @@
+export const data = [
+	{
+		id: 1,
+		img: 'src/img/render-libro-familia-2.webp',
+		nameProduct: 'Mejor una Familia',
+		autor: 'Fabian Ruiz',
+		price: 1800,
+		quantity: 1,
+	},
+	{
+		id: 2,
+		img: 'src/img/render-libro-familia-21.webp',
+		nameProduct: '¡Bienaventurados!',
+		autor: 'Fabian Ruiz',
+		price: 1200,
+		quantity: 1,
+	},
+	{
+		id: 3,
+		img: 'src/img/render-tormentas-financieras.webp',
+		nameProduct: 'Tormentas Financieras',
+		autor: 'Fabian Ruiz',
+		price: 1300,
+		quantity: 1,
+	},
+	{
+		id: 4,
+		img: 'src/img/render-una-sola-pasion.webp',
+		nameProduct: 'Una Sola Pasión',
+		autor: 'Fabian Ruiz',
+		price: 1500,
+		quantity: 1,
+	},
+	{
+		id: 5,
+		img: 'src/img/render-padres-de-valor.webp',
+		nameProduct: 'Padres de Valor',
+		autor: 'Fabian Ruiz',
+		price: 2000,
+		quantity: 1,
+	},
+	// Aquí pueden agregar más o modificar los productos existentes
+];
